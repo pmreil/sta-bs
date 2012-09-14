59899c994ac5f2c9055efdced540f5ebddf158c7
Paul Reilly <preilly@teladoc.com>
2012-09-13 22:22:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sta::Application.config.secret_token = '10f3fe22e73170a4ec92a44bf517d2e3b8f3bce64939243037182479545339550dab78e2d3aa716abe470d2de069340c9c339acb86aac0ac9e6096b24ba12dc5'
